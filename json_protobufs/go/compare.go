6fb859292578291e360bf3fca0456c9719991266
ivan kavtarev <ivan.kavtarev@mail.ru>
2025-01-20 10:05:54 +0300

    clean

--- json_protobufs/go/compare.go
+++ json_protobufs/go/compare.go
@@ -11,57 +11,52 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+var numOfRuns = 10000
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: go run main.go <level>")
 	}
 	level := os.Args[1]
 
-	// Читаем JSON файл
 	jsonFilePath := filepath.Join("..", "common", "json", fmt.Sprintf("%s.json", level))
 	jsonDataBytes, err := os.ReadFile(jsonFilePath)
 	if err != nil {
 		log.Fatalf("Failed to read JSON file: %v", err)
 	}
 
-	// Парсим JSON
-	var jsonData Large // Структура из Protobuf
+	var jsonData Large
 	err = json.Unmarshal(jsonDataBytes, &jsonData)
 	if err != nil {
 		log.Fatalf("Failed to unmarshal JSON: %v", err)
 	}
 
-	// Кодируем в Protobuf
 	protoData, err := proto.Marshal(&jsonData)
 	if err != nil {
 		log.Fatalf("Failed to marshal Protobuf: %v", err)
 	}
 
-	// Benchmark JSON Serialize
 	start := time.Now()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < numOfRuns; i++ {
 		_, _ = json.Marshal(jsonData)
 	}
 	fmt.Printf("JSON Serialize: %v\n", time.Since(start))
 
-	// Benchmark Protobuf Serialize
 	start = time.Now()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < numOfRuns; i++ {
 		_, _ = proto.Marshal(&jsonData)
 	}
 	fmt.Printf("Protobuf Serialize: %v\n", time.Since(start))
 
-	// Benchmark JSON Parse
 	start = time.Now()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < numOfRuns; i++ {
 		var tmp Large
 		_ = json.Unmarshal(jsonDataBytes, &tmp)
 	}
 	fmt.Printf("JSON Parse: %v\n", time.Since(start))
 
-	// Benchmark Protobuf Parse
 	start = time.Now()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < numOfRuns; i++ {
 		var tmp Large
 		_ = proto.Unmarshal(protoData, &tmp)
 	}
--- json_protobufs/node/clean-compare.js
+++ json_protobufs/node/clean-compare.js
@@ -2,6 +2,8 @@ const protobuf = require('protobufjs');
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
+const numOfRuns = 10000
+
 async function benchmarkSerialization() {
   const { 2: level } = process.argv
   if (!level) throw "level needed";
@@ -14,25 +16,25 @@ async function benchmarkSerialization() {
   const protoData = buffer.encode(jsonData).finish();
 
   console.time('JSON Serialize');
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < numOfRuns; i++) {
     JSON.stringify(jsonData);
   }
   console.timeEnd('JSON Serialize');
 
   console.time('Protobuf Serialize');
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < numOfRuns; i++) {
     buffer.encode(jsonData).finish();
   }
   console.timeEnd('Protobuf Serialize');
 
   console.time('JSON Parse');
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < numOfRuns; i++) {
     JSON.parse(jsonDataSting);
   }
   console.timeEnd('JSON Parse');
 
   console.time('Protobuf Parse');
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < numOfRuns; i++) {
     buffer.decode(protoData);
   }
   console.timeEnd('Protobuf Parse');
