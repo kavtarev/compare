0a499535cd0c9a9a31d5ddee6a0c5f2236d6d152
ivan kavtarev <ivan.kavtarev@mail.ru>
2025-07-15 23:39:01 +0300

    add space

--- http_nats_rabbit_grpc/go/sender/nats_handler.go
+++ http_nats_rabbit_grpc/go/sender/nats_handler.go
@@ -9,11 +9,13 @@ import (
 
 func (s *Server) NatsHandler(w http.ResponseWriter, r *http.Request) {
 	input := s.GetStructByInput()
+
 	for i := 0; i < s.opts.AmountOfObjects; i++ {
 		obj, err := json.Marshal(input)
 		if err != nil {
 			fmt.Println("cant marshal json")
 		}
+
 		s.nc.Publish("init", obj)
 	}
 
